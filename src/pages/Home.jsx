76e849b283a1d72f8199f6cd94633d711bf16204
Jhan00 <Jhandiazabad@gmail.com>
2022-05-20 12:08:47 -0400

    Adding process into github. updating pages with react

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const RecoveryPassword = () => {
+	return (
+		<div>
+            <h1>React Shop</h1>
+        </div>
+	);
+}
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const RecoveryPassword = () => {
+	return (
+		<div>
+    Not Found, error 404.
+        </div>
+	);
+}
+
+export default NotFound;
