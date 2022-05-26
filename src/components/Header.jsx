1242d44d93f7f7b7597bdb59502aa5eee9df766a
Jhan00 <Jhandiazabad@gmail.com>
2022-05-26 07:16:16 -0400

    fixing styles, adding hooks to modify and better code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,57 @@
-import React from 'react';
-import '../styles/Header.scss';
+import React, { useState } from 'react';
+import '@styles/Header.scss';
+import Menu from '@components/Menu';
+import menu from '@icons/menu-bar.png';
+import logo from '@logo/main-logo.jpeg';
+import shoppingCart from '@icons/cart.png';
 
 const Header = () => {
-    return (
-        <nav>
-        <img src="./icons/menu-bar.png" alt="" className="menu" />
-        <div className="navbar-left">
-            <img src="./logo/main-logo.jpeg" alt="logo" className="logo" />
-            <ul>
-                <li>
-                    <a href="/">All</a>
-                </li>
-                <li>
-                    <a href="/">Clothes</a>
-                </li>
-                <li>
-                    <a href="/">Electronics</a>
-                </li>
-                <li>
-                    <a href="/">Furniture</a>
-                </li>
-                <li>
-                    <a href="/">Toys</a>
-                </li>
-                <li>
-                    <a href="/">Others</a>
-                </li>
-            </ul>
-        </div>
-        <div className="navbar-right">
-            <ul>
-                <li className="navbar-email">platzi@example.com</li>
-                <li className="navbar-shopping-cart">
-                    <img src="./icons/cart.png" alt="shopping cart" />
-                    <div>2</div>
-                </li>
-                </ul>
-        </div>
-    </nav>
-    );
+	const [toggle, setToggle] = useState(false);
+
+	const handleToggle = () => {
+		setToggle(!toggle);
+	}
+
+	return (
+		<nav>
+			<img src={menu} alt="menu" className="menu" />
+			<div className="navbar-left">
+				<img src={logo} alt="logo" className="nav-logo" />
+				<ul>
+					<li>
+						<a href="/">All</a>
+					</li>
+					<li>
+						<a href="/">Clothes</a>
+					</li>
+					<li>
+						<a href="/">Electronics</a>
+					</li>
+					<li>
+						<a href="/">Furniture</a>
+					</li>
+					<li>
+						<a href="/">Toys</a>
+					</li>
+					<li>
+						<a href="/">Others</a>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-right">
+				<ul>
+					<li className="navbar-email" onClick={handleToggle}>
+						platzi@example.com
+					</li>
+					<li className="navbar-shopping-cart">
+						<img src={shoppingCart} alt="shopping cart" />
+						<div>2</div>
+					</li>
+				</ul>
+			</div>
+			{toggle && <Menu />}
+		</nav>
+	);
 }
 
 export default Header;
